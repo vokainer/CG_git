4e59ff15986a2b7d1832274b8f1d4599df97df7a
EndOfFile <towbow@gmx.de>
2011-06-02 22:17:09 +0200

    More Comments

--- clock.cpp
+++ clock.cpp
@@ -7,6 +7,7 @@ Clock::Clock(Vec3d center, int radius)
     m_center = center;
     m_radius = radius;
 
+    // Start Position of all Pointers
     m_hours = Vec3d(0,center.getY() + radius - 30,1);
     m_minutes = Vec3d(0,center.getY() + radius - 10,1);
     m_seconds = Vec3d(0,center.getY() + radius,1);
@@ -14,6 +15,7 @@ Clock::Clock(Vec3d center, int radius)
     m_RotationSeconds = Mat3d::getRotationmatrix(-6*3.141592/180); //Rotationmatrix of -6°
     m_RotationHours = Mat3d::getRotationmatrix(-30*3.141592/180); //Rotationmatrix of -30°
 
+    // Initialize Counters
     m_militime = 0;
     m_SCount = 0;
     m_MCount = 0;
@@ -23,6 +25,8 @@ Clock::Clock(Vec3d center, int radius)
     timestamp = time(0);
     m_now = localtime(&timestamp);
 
+
+    // Set Clock to current time
     if(m_now->tm_hour != 0)
         for(int h=0; h < m_now->tm_hour; h++)
             m_hours = m_RotationHours * m_hours;
@@ -47,16 +51,17 @@ void Clock::update(int elapsed)
         m_militime += 100;
 
     //qDebug() << militime << endl;
+    // Check if 1 Second passed
     if((m_militime % 1000) == 0 && m_militime != 0){
         m_seconds = m_RotationSeconds * m_seconds;
         m_militime = 0;
         m_SCount++;
-
+        //After 60 Seconds move Minute Pointer
         if(m_SCount >= 60){
             m_minutes = m_RotationSeconds * m_minutes;
             m_SCount = 0;
             m_MCount++;
-
+            //After 60 Minutes move Hour Pointer
             if(m_MCount >= 60){
                 m_hours = m_RotationHours * m_hours;
                 m_MCount = 0;
@@ -64,7 +69,6 @@ void Clock::update(int elapsed)
         }
     }
 
-    //m_hours = Mat3d::getRotationmatrix(45)*m_hours;
 }
 
 
