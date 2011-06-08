abca29af91d4a09e421fe553d0c8e10c15507300
EndOfFile <towbow@gmx.de>
2011-06-08 17:58:18 +0200

    Rotation beliebige Achse

--- matrix.h
+++ matrix.h
@@ -246,6 +246,169 @@ public:
         return translate;
     }
 
+    // Get Rotationmatrix for Z-Axes
+    static Matrix<double, 4> getRotationmatrixZ(double degree){
+        degree = degree*3.141592/180;
+        Matrix<double, 4> rotate;
+        rotate(0,0) = cos(degree);
+        rotate(0,1) = -sin(degree);
+        rotate(1,0) = sin(degree);
+        rotate(1,1) = cos(degree);
+        rotate(2,2) = 1.0d;
+        rotate(3,3) = 1.0d;
+        return rotate;
+    }
+
+    // Get Rotationmatrix for X-Axes
+    static Matrix<double, 4> getRotationmatrixX(double degree){
+        degree = degree*3.141592/180;
+        Matrix<double, 4> rotate;
+        rotate(0,0) = 1.0d;
+        rotate(1,1) = cos(degree);
+        rotate(1,2) = -sin(degree);
+        rotate(2,1) = sin(degree);
+        rotate(2,2) = cos(degree);
+        rotate(3,3) = 1.0d;
+        return rotate;
+    }
+
+    // Get Rotationmatrix for Y-Axes
+    static Matrix<double, 4> getRotationmatrixY(double degree){
+        degree = degree*3.141592/180;
+        Matrix<double, 4> rotate;
+        rotate(0,0) = cos(degree);
+        rotate(0,2) = sin(degree);
+        rotate(1,1) = 1.0d;
+        rotate(2,0) = -sin(degree);
+        rotate(2,2) = cos(degree);
+        rotate(3,3) = 1.0d;
+        return rotate;
+    }
+
+    // Rotation beliebige Achse
+    static Matrix<double, 4> getRotation(Vec4d oldVector, double degree){
+        Matrix<double, 4> rotation;
+        Matrix<double, 4> solution;
+        double d = sqrt(oldVector(0)^2 + oldVector(1)^2);
+
+        //Rz
+        rotation(0,0) = oldVector(0);
+        rotation(0,1) = oldVector(1);
+        rotation(0,2) = 0;
+        rotation(0,3) = 0;
+
+        rotation(1,0) = -oldVector(1);
+        rotation(1,1) = oldVector(0);
+        rotation(1,2) = 0;
+        rotation(1,3) = 0;
+
+        rotation(2,0) = 0;
+        rotation(2,1) = 0;
+        rotation(2,2) = d;
+        rotation(2,3) = 0;
+
+        rotation(3,0) = 0;
+        rotation(3,1) = 0;
+        rotation(3,2) = 0;
+        rotation(3,3) = d;
+
+        solution = 1/d * rotation;
+
+        //Ry
+        rotation(0,0) = oldVector(2);
+        rotation(0,1) = 0;
+        rotation(0,2) = -d;
+        rotation(0,3) = 0;
+
+        rotation(1,0) = 0;
+        rotation(1,1) = 1;
+        rotation(1,2) = 0;
+        rotation(1,3) = 0;
+
+        rotation(2,0) = d;
+        rotation(2,1) = 0;
+        rotation(2,2) = oldVector(2);
+        rotation(2,3) = 0;
+
+        rotation(3,0) = 0;
+        rotation(3,1) = 0;
+        rotation(3,2) = 0;
+        rotation(3,3) = 1;
+
+        solution *= rotation;
+
+        //Rotation Rz(degree)
+        rotation(0,0) = cos(degree);
+        rotation(0,1) = -sin(degree);
+        rotation(0,2) = 0;
+        rotation(0,3) = 0;
+
+        rotation(1,0) = sin(degree);
+        rotation(1,1) = cos(degree);
+        rotation(1,2) = 0;
+        rotation(1,3) = 0;
+
+        rotation(2,0) = 0;
+        rotation(2,1) = 0;
+        rotation(2,2) = 1;
+        rotation(2,3) = 0;
+
+        rotation(3,0) = 0;
+        rotation(3,1) = 0;
+        rotation(3,2) = 0;
+        rotation(3,3) = 1;
+
+        solution *= rotation;
+
+        //Ry reverse
+        rotation(0,0) = oldVector(2);
+        rotation(0,1) = 0;
+        rotation(0,2) = d;
+        rotation(0,3) = 0;
+
+        rotation(1,0) = 0;
+        rotation(1,1) = 1;
+        rotation(1,2) = 0;
+        rotation(1,3) = 0;
+
+        rotation(2,0) = -d;
+        rotation(2,1) = 0;
+        rotation(2,2) = oldVector(2);
+        rotation(2,3) = 0;
+
+        rotation(3,0) = 0;
+        rotation(3,1) = 0;
+        rotation(3,2) = 0;
+        rotation(3,3) = 1;
+
+        solution *= rotation;
+
+        //Rz reverse
+        rotation(0,0) = oldVector(0);
+        rotation(0,1) = -oldVector(1);
+        rotation(0,2) = 0;
+        rotation(0,3) = 0;
+
+        rotation(1,0) = oldVector(1);
+        rotation(1,1) = oldVector(0);
+        rotation(1,2) = 0;
+        rotation(1,3) = 0;
+
+        rotation(2,0) = 0;
+        rotation(2,1) = 0;
+        rotation(2,2) = d;
+        rotation(2,3) = 0;
+
+        rotation(3,0) = 0;
+        rotation(3,1) = 0;
+        rotation(3,2) = 0;
+        rotation(3,3) = d;
+
+        solution *= 1/d *rotation;
+
+        return solution;
+    }
+
 private:
     T m_data[SIZE][SIZE];
 };
